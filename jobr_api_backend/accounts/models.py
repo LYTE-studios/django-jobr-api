f842c80ddf74ee2f796059ab386e50b26177162f
FutureSea <futuresea.dev@gmail.com>
2024-11-27 15:50:30 +0200

    Specify a user Type and let a user have a specific type of profile based on that type. The 3 types are "Employee" & "Employer" & "Admin"

--- jobr_api_backend/accounts/models.py
+++ jobr_api_backend/accounts/models.py
@@ -4,3 +4,31 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
+
+
+class Employee(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    date_of_birth = models.DateField()
+    gender = models.CharField(max_length=10, choices=[('male', 'Male'), ('female', 'Female'), ('other', 'Other')])
+    phone_number = models.CharField(max_length=15)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    city_name = models.CharField(max_length=100, blank=True, null=True)
+    biography = models.TextField(blank=True, null=True)
+
+
+class Employer(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    vat_number = models.CharField(max_length=30)
+    company_name = models.CharField(max_length=100)
+    street_name = models.CharField(max_length=100)
+    house_number = models.CharField(max_length=10)
+    city = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=20)
+    coordinates = models.JSONField()  # Stores latitude and longitude as JSON
+    website = models.URLField(blank=True, null=True)
+    biography = models.TextField(blank=True, null=True)
+
+
+class Admin(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=100)
--- jobr_api_backend/accounts/urls.py
+++ jobr_api_backend/accounts/urls.py
@@ -1,9 +1,12 @@
 # accounts/urls.py
 from django.urls import path
-from .views import UserRegistrationView, UserLoginView, UserDetailView
+from .views import UserRegistrationView, UserLoginView, UserDetailView, EmployeeRegistration, EmployerRegistration, AdminRegistration
 
 urlpatterns = [
-    path('register/', UserRegistrationView.as_view(), name='register'),
+    # path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),  # Added user detail URL
+    path('register/employee/', EmployeeRegistration.as_view(), name='employee-registration'),
+    path('register/employer/', EmployerRegistration.as_view(), name='employer-registration'),
+    path('register/admin/', AdminRegistration.as_view(), name='admin-registration'),
 ]

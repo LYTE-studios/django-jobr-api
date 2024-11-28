726ba9ff8a5e7f8ae7c2feb2e429668d6dd79a29
Techie John <ayodelejohn40@gmail.com>
2024-11-28 14:23:20 +0100

    Implemented Google and Apple sign-in

--- jobr_api_backend/accounts/firebase_utils.py
+++ jobr_api_backend/accounts/firebase_utils.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, auth
 
 def initialize_firebase():
-    cred = credentials.Certificate('firebase_secrets.json')
+    cred = credentials.Certificate('firebase-secrets.json')
     firebase_admin.initialize_app(cred)
 
 initialize_firebase()

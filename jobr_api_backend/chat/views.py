81297456cfbf37b6495bdb5212d080b9f0390d86
manong02 <manon.golos@gmail.com>
2025-03-10 11:54:43 +0100

    added doc for GetMessagesView and GetChatRoomListView

--- jobr_api_backend/chat/views.py
+++ jobr_api_backend/chat/views.py
@@ -87,10 +87,46 @@ class SendMessageView(APIView):
 
 
 class GetMessagesView(APIView):
+
+    """
+    View to retrieve messages from a chat room or between two users.
+
+    This view requires the user to be authenticated via JWT authentication. It handles 
+    the retrieval of messages from a chat room. The messages can be fetched by specifying 
+    either the `chatroom_id` or `user_id`.
+
+    Authentication:
+        Requires a valid JWT token for access.
+
+    Permissions:
+        Requires the user to be authenticated (`IsAuthenticated`).
+
+    Methods:
+        GET: Retrieves all messages in a chat room or between the authenticated user 
+        and another user. Marks unread messages as read.
+    """
+
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get(self, request, chatroom_id=None, user_id=None):
+
+        """
+        Retrieve all messages for a specified chat room or between two users.
+
+        Args:
+            request (Request): The HTTP request object containing the request data.
+            chatroom_id (int): The ID of the chat room to fetch messages from.
+            user_id (int): The ID of the user to fetch messages from.
+
+        Returns:
+            Response: A response object containing the serialized message data.
+
+        Raises:
+            Response: If neither `chatroom_id` nor `user_id` is provided, or if a 
+            chat room for the specified `user_id` does not exist.
+        """
+
         if chatroom_id:
             chat_room = get_object_or_404(ChatRoom, id=chatroom_id)
         elif user_id:
@@ -118,10 +154,41 @@ class GetMessagesView(APIView):
 
 
 class GetChatRoomListView(APIView):
+
+    """
+    View to retrieve a list of chat rooms for the authenticated user.
+
+    This view requires the user to be authenticated via JWT authentication. It fetches 
+    a list of chat rooms for the authenticated user and returns details about each chat 
+    room, including the last message, unread message count, and information about the other user in the chat room.
+
+    Authentication:
+        Requires a valid JWT token for access.
+
+    Permissions:
+        Requires the user to be authenticated (`IsAuthenticated`).
+
+    Methods:
+        GET: Retrieves a list of chat rooms for the authenticated user, including details 
+        about the last message, unread messages count, and the other user in the chat room.
+    """
+
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
+
+        """
+        Retrieve a list of chat rooms for the authenticated user, including details about 
+        the last message, unread messages count, and the other user in each chat room.
+
+        Args:
+            request (Request): The HTTP request object containing the request data.
+
+        Returns:
+            Response: A response object containing the list of chat rooms and their details.
+        """
+        
         # Get all chat rooms for a user and order by the latest message
         chat_rooms = (
             ChatRoom.objects.filter(users=request.user)

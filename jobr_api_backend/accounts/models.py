386b16c3da44ee27c1d2246413995145dae12f39
Tanguy De Brabandere <125645329+TanguyDeBrabandere@users.noreply.github.com>
2025-04-08 16:27:32 +0200

    rm lat lon

--- jobr_api_backend/accounts/models.py
+++ jobr_api_backend/accounts/models.py
@@ -34,8 +34,6 @@ class Employee(models.Model):
     phone_number = models.CharField(max_length=15, null=True, blank=True, default=None)
     city_name = models.CharField(max_length=100, blank=True, null=True)
     biography = models.TextField(blank=True, null=True)
-    latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
-    longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
     phone_session_counts = models.IntegerField(default=0)
     availability_status = models.CharField(
         max_length=20,
